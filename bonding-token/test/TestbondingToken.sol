ab7f6f0a6f2876afc0344036557f8027efca351a
Vladimir Trifonov <trifonov.vp@gmail.com>
2023-04-18 22:12:14 +0300

    test: add more bonding-token echidna fuzz tests

--- bonding-token/test/TestbondingToken.sol
+++ bonding-token/test/TestbondingToken.sol
@@ -12,6 +12,7 @@ contract TestBondingToken {
     BondingToken bondingToken;
 
     event Balance(uint256 amount, uint256 price, uint256 balance);
+    event Payout(uint256 amount, uint256 tokens, uint256 decoded);
 
     constructor() payable {
         bondingToken = new BondingToken("TestBondingToken", "TBT");
@@ -71,6 +72,50 @@ contract TestBondingToken {
 
         assert(bondingToken.balanceOf(address(this)) == initialBalance);
         assert(initialEth == address(this).balance);
+    }
+
+    function test_payout(uint256 amount) public {
+        amount = 1 + (amount % 100);
+        uint256 initSupply = bondingToken.totalSupply();
+        uint256 balance = bondingToken.balanceOf(address(this));
+        uint256 ethBalance = address(this).balance;
+        uint256 tokenEthBalance = address(bondingToken).balance;
+
+        if (amount == 0) {
+            amount = 1 + (amount % ethBalance);
+        } else {
+            amount = amount * 10 ** 18;
+        }
+        uint256 tokens = bondingToken.calculateTokensForPrice(amount);
+
+        require(tokens > 0 && tokens <= bondingToken.MAX_BUY_AMOUNT_PER_TX());
+
+        (bool success, bytes memory output) = address(bondingToken).call{
+            value: amount
+        }(abi.encode(tokens));
+        uint256 bought = bondingToken.balanceOf(address(this)) - balance;
+        uint256 cost = bondingToken.calculatePriceForTokens(bought, initSupply);
+        uint256 decoded = abi.decode(output, (uint256));
+
+        emit Payout(amount, tokens, decoded);
+
+        assert(success);
+        assert(decoded == tokens);
+        assert(address(this).balance == ethBalance - cost);
+        assert(address(bondingToken).balance == tokenEthBalance + cost);
+    }
+
+    function test_transfer(uint256 amount) public {
+        uint256 balance = bondingToken.balanceOf(address(this));
+        amount = 1 + (amount % balance);
+        uint256 ethBalance = address(this).balance;
+        require(amount > 0, "balance is zero");
+
+        bondingToken.transfer(address(bondingToken), amount);
+        uint256 payout = bondingToken.calculatePriceForTokens(amount);
+
+        assert(bondingToken.balanceOf(address(this)) == balance - amount);
+        assert(address(this).balance == ethBalance + payout);
     }
 
     receive() external payable {}
